82a485b9a24c096768b464058668ac18ae60830b
Ian Torres <ian@zentrack.cl>
2024-03-18 18:21:47 -0300

    Websocket testing added.

--- test/source/server.cpp
+++ test/source/server.cpp
@@ -43,46 +43,71 @@ TEST_CASE("Server respond http request") {
   thread.detach();
 
   boost::asio::io_context ioc;
-
-  // These objects perform our I/O
   boost::asio::ip::tcp::resolver resolver(ioc);
   boost::beast::tcp_stream stream(ioc);
 
-  // Look up the domain name
   auto const results = resolver.resolve("localhost", arg_2);
 
-  // Make the connection on the IP address we get from a lookup
   stream.connect(results);
 
-  // Set up an HTTP GET request message
   boost::beast::http::request<boost::beast::http::string_body> req{boost::beast::http::verb::get,
                                                                    "/not-found", 10};
   req.set(boost::beast::http::field::host, "localhost");
   req.set(boost::beast::http::field::user_agent, BOOST_BEAST_VERSION_STRING);
 
-  // Send the HTTP request to the remote host
   boost::beast::http::write(stream, req);
 
-  // This buffer is used for reading and must be persisted
   boost::beast::flat_buffer buffer;
 
-  // Declare a container to hold the response
   boost::beast::http::response<boost::beast::http::dynamic_body> res;
 
-  // Receive the HTTP response
   boost::beast::http::read(stream, buffer, res);
 
-  res.prepare_payload();
-
   auto output = boost::beast::buffers_to_string(res.body().data());
-
   CHECK_EQ(output, std::string("The resource '/not-found' was not found."));
 
   boost::beast::error_code ec;
   stream.socket().shutdown(boost::asio::ip::tcp::socket::shutdown_both, ec);
 
-  // not_connected happens sometimes
-  // so don't bother reporting it.
-  //
   if (ec && ec != boost::beast::errc::not_connected) throw boost::beast::system_error{ec};
+}
+
+TEST_CASE("Server respond websocket request") {
+  using namespace server;
+  Server server;
+  char arg_1[] = "0.0.0.0";
+  char arg_2[] = "9000";
+  char arg_3[] = ".";
+  char arg_4[] = "1";
+  char arg_5[] = "run";
+  char* argv[] = {nullptr, arg_1, arg_2, arg_3, arg_4, arg_5};
+
+  auto thread = std::thread([&]() { server.run(argv); });
+  thread.detach();
+
+  boost::asio::io_context ioc;
+
+  boost::asio::ip::tcp::resolver resolver{ioc};
+  boost::beast::websocket::stream<boost::asio::ip::tcp::socket> ws{ioc};
+
+  auto const results = resolver.resolve("localhost", "9000");
+
+  boost::asio::connect(ws.next_layer(), results);
+
+  ws.set_option(boost::beast::websocket::stream_base::decorator(
+      [](boost::beast::websocket::request_type& req) {
+        req.set(boost::beast::http::field::user_agent, std::string(BOOST_BEAST_VERSION_STRING));
+      }));
+
+  ws.handshake("localhost:9000", "/");
+
+  ws.write(boost::asio::buffer(std::string("hello")));
+
+  boost::beast::flat_buffer buffer;
+
+  ws.read(buffer);
+  ws.close(boost::beast::websocket::close_code::normal);
+
+  auto output = boost::beast::buffers_to_string(buffer.data());
+  CHECK_EQ(output, std::string("hello"));
 }
