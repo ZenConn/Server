9a4e6bf52843610dbe72b552a360a8a2ddb650db
Ian Torres <ian@zentrack.cl>
2024-03-20 17:59:44 -0300

    Uuid added.

--- include/server/session.h
+++ include/server/session.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#include <boost/uuid/uuid_generators.hpp>
 #include <memory>
 
-class session : public std::enable_shared_from_this<session> {};
+class session : public std::enable_shared_from_this<session> {
+public:
+  boost::uuids::uuid uuid_;
+  session() : uuid_(boost::uuids::random_generator()()) {}
+};
