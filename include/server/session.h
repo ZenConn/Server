5dfb1b2c73ce8880b1c42c38db52702498b456c7
Ian Torres <ian@zentrack.cl>
2024-03-20 18:03:33 -0300

    Uuid can be accessed via get_uuid()

--- include/server/session.h
+++ include/server/session.h
@@ -1,10 +1,13 @@
 #pragma once
 
 #include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
 #include <memory>
 
 class session : public std::enable_shared_from_this<session> {
-public:
   boost::uuids::uuid uuid_;
+
+public:
   session() : uuid_(boost::uuids::random_generator()()) {}
+  std::string get_uuid() { return boost::uuids::to_string(uuid_); }
 };
