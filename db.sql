e8ba890ad2f1b86247404b2472476db7fa836e4f
Ian Torres <iantorres@outlook.com>
2024-03-27 03:02:30 -0300

    Fix no such file or directory.

---
+++ db.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE zenconn;
+
+USE zenconn;
+
+create table servers (
+    uuid char(36) not null primary key,
+    shutdown_at timestamp null,
+    created_at timestamp null,
+    updated_at timestamp null
+) collate = utf8mb4_unicode_ci;
