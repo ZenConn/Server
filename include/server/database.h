1421147a8f3651aa018d321da6607e0018449f7b
Ian Torres <iantorres@outlook.com>
2024-03-28 06:30:50 -0300

    Rollback.

--- include/server/database.h
+++ include/server/database.h
@@ -16,6 +16,17 @@
 
 #include "session.h"
 
+class error {
+public:
+  void static print(const boost::mysql::error_with_diagnostics& error) {
+    std::string name = boost::uuids::to_string(boost::uuids::random_generator()()) + ".error_log";
+    std::ofstream outfile(name.data());
+    outfile << BOOST_CURRENT_FUNCTION << ": " << error.what() << '\n'
+            << "Server diagnostics: " << error.get_diagnostics().server_message() << std::endl;
+    outfile.close();
+  }
+};
+
 class database_connection {
   boost::asio::io_context ioc_;
   boost::asio::ssl::context ssl_ioc_;
@@ -35,7 +46,11 @@ public:
     boost::mysql::handshake_params database_params(database_config.at("username").as_string(),
                                                    database_config.at("password").as_string(),
                                                    database_config.at("name").as_string());
-    sock.connect(*endpoints.begin(), database_params);
+    try {
+      sock.connect(*endpoints.begin(), database_params);
+    } catch (const boost::mysql::error_with_diagnostics& err) {
+      error::print(err);
+    }
   }
 };
 
@@ -47,37 +62,54 @@ public:
 
   void server_start(boost::uuids::uuid& uuid) {
     auto conn = std::make_shared<database_connection>(config_);
-    boost::mysql::string_view query = "INSERT INTO servers (uuid) VALUES (?)";
-    auto statement = conn->sock.prepare_statement(query);
-    boost::mysql::results result;
-    conn->sock.execute(statement.bind(boost::uuids::to_string(uuid)), result);
-    conn->sock.close_statement(statement);
+    try {
+      boost::mysql::string_view query = "INSERT INTO servers (uuid) VALUES (?)";
+      auto statement = conn->sock.prepare_statement(query);
+      boost::mysql::results result;
+      conn->sock.execute(statement.bind(boost::uuids::to_string(uuid)), result);
+      conn->sock.close_statement(statement);
+    } catch (const boost::mysql::error_with_diagnostics& err) {
+      error::print(err);
+    }
   }
 
   void server_shutdown(boost::uuids::uuid& uuid) {
     auto conn = std::make_shared<database_connection>(config_);
-    boost::mysql::string_view query = "UPDATE servers SET shutdown_at = now() WHERE uuid = ?";
-    auto statement = conn->sock.prepare_statement(query);
-    boost::mysql::results result;
-    conn->sock.execute(statement.bind(boost::uuids::to_string(uuid)), result);
-    conn->sock.close_statement(statement);
+    try {
+      boost::mysql::string_view query = "UPDATE servers SET shutdown_at = now() WHERE uuid = ?";
+      auto statement = conn->sock.prepare_statement(query);
+      boost::mysql::results result;
+      conn->sock.execute(statement.bind(boost::uuids::to_string(uuid)), result);
+      conn->sock.close_statement(statement);
+    } catch (const boost::mysql::error_with_diagnostics& err) {
+      error::print(err);
+    }
   }
 
   void add_session(std::shared_ptr<session>& session) {
     auto conn = std::make_shared<database_connection>(config_);
-    boost::mysql::string_view query = "INSERT INTO sessions (uuid) VALUES (?)";
-    auto statement = conn->sock.prepare_statement(query);
-    boost::mysql::results result;
-    conn->sock.execute(statement.bind(session->get_uuid()), result);
-    conn->sock.close_statement(statement);
+    try {
+      boost::mysql::string_view query = "INSERT INTO sessions (uuid) VALUES (?)";
+      auto statement = conn->sock.prepare_statement(query);
+      boost::mysql::results result;
+      conn->sock.execute(statement.bind(session->get_uuid()), result);
+      conn->sock.close_statement(statement);
+    } catch (const boost::mysql::error_with_diagnostics& err) {
+      error::print(err);
+    }
   }
 
   void remove_session(std::shared_ptr<session>& session) {
     auto conn = std::make_shared<database_connection>(config_);
-    boost::mysql::string_view query = "UPDATE sessions SET disconnected_at = now() WHERE uuid = ?";
-    auto statement = conn->sock.prepare_statement(query);
-    boost::mysql::results result;
-    conn->sock.execute(statement.bind(session->get_uuid()), result);
-    conn->sock.close_statement(statement);
+    try {
+      boost::mysql::string_view query
+          = "UPDATE sessions SET disconnected_at = now() WHERE uuid = ?";
+      auto statement = conn->sock.prepare_statement(query);
+      boost::mysql::results result;
+      conn->sock.execute(statement.bind(session->get_uuid()), result);
+      conn->sock.close_statement(statement);
+    } catch (const boost::mysql::error_with_diagnostics& err) {
+      error::print(err);
+    }
   }
 };
